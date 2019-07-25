0767120a3df1f008af9ad92545a0e6c42805808d
zhaoyang <1320587103@qq.com>
2019-07-25 09:38:45 +0800

    图表组件

--- src/components/ECharts /Bar.jsx
+++ src/components/ECharts /Bar.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import echarts from 'echarts';
 
-// 折线图组件
-export default class ComLine extends React.Component{
+// 柱状图组件
+export default class ComBar extends React.Component{
     static defaultProps = {
-        Id:'line', //实例化dom，每个图表必须唯一
-        lineTitle:'折线图实例', //示例的标题
+        Id:'bar', //实例化dom，每个图表必须唯一
+        lineTitle:'柱状图实例', //示例的标题
         xAxisData:["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],  //示例的x轴数据
         seriesName:'test', //数据的所有的标题
-        seriesType:'line', //示例的类型
-        seriesColor:'pink', //折线示例的颜色
+        seriesType:'bar', //示例的类型
         seriesData:[  //展示的数据
             {value:25, name:'衬衫2',itemStyle: {
                 color: '#041527'
@@ -37,7 +36,6 @@ export default class ComLine extends React.Component{
             },
             yAxis: {},
             series: [{
-                color:this.props.seriesColor,
                 name: this.props.seriesName,
                 type: this.props.seriesType,
                 data: this.props.seriesData,
@@ -47,7 +45,7 @@ export default class ComLine extends React.Component{
 
     render(){
         return (
-            <div id={this.props.Id} style={{ width: '100%', height: '100%' }}></div>
+            <div id={this.props.Id} style={{ width: '100%', height: '100%' }}></div>            
         )
     }
 }
