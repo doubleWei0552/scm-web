3e7253a3cb8afc66a46e549b81fe26e01b172714
zhaoyang <1320587103@qq.com>
2019-08-17 10:11:19 +0800

    导向页时间格式分成起始和结束类型

--- src/components/DetailPage/Index.jsx
+++ src/components/DetailPage/Index.jsx
@@ -399,7 +399,7 @@ class DetailPage extends PureComponent {
                                     {...formItemLayout}
                                   >
                                     {getFieldDecorator(`${field.FIELD_NAME}`, {
-                                      initialValue: _.get(field, 'FIELD_VALUE') || '',
+                                      initialValue: _.get(field, 'FIELD_VALUE'),
                                       rules: [
                                         {
                                           required: field.REQUIRED_CONDITION,
