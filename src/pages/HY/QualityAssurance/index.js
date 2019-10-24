59be99839bd2f5a53814810a7b260ff6cae9176a
weiwei <weiwei@qq.com>
2019-10-24 19:21:21 +0800

    联调测试

--- src/pages/HY/QualityAssurance/index.js
+++ src/pages/HY/QualityAssurance/index.js
@@ -364,7 +364,7 @@ export default class QualityAssurance extends React.Component {
         dataIndex: 'QUALITY_STATUS',
         key: 'CheckState',
         disabled: true,
-        isMultiple: false,
+        isMultiple: true,
         className: 'nocolor',
         options: [{ text: "待检", value: "0" },
         { text: "合格", value: "1" },
